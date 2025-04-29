28c70c4f59aa94577a5bd233a173f95a21f1f282
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-04-29 17:06:31 +0330

    edit and delete add new function

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-def add_new(ls, sum):
-    new_ls = []
-    i = 0
-    while i < len(ls) and ls[i] > sum:
-        new_ls.append(ls[i])
-        i += 1
-    new_ls.append(sum)
-    while i < len(ls):
-        new_ls.append(ls[i])
-        i += 1
-    return new_ls
-
-
+import heapq
 
 n = int(input())
 ls = list(map(int, input().split()))
 
-ls.sort(reverse=True)
+heapq.heapify(ls)
 
 cost = 0
 while len(ls) > 1:
-    print(ls)
-    x = ls.pop()
-    y = ls.pop()
+    x = heapq.heappop(ls)
+    y = heapq.heappop(ls)
     sum = x + y
     cost += sum
-    ls = add_new(ls, sum)
+    heapq.heappush(ls, sum)
 
 print(cost)
